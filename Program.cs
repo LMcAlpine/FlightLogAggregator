92de935a9cd4b65d74379a377d2610166be52ee1
Luke <lukedmcalpine@gmail.com>
2025-05-25 21:19:14 -0700

    Process the csv

--- Program.cs
+++ Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        const string CSVFile = "T_ONTIME_REPORTING.csv";
+        if (!File.Exists(CSVFile))
+        {
+            Console.WriteLine($"CSV file '{CSVFile}' not found.");
+            return;
+        }
+
+        string[] headers;
+        var values = new List<string[]>();
+        using (var reader = new StreamReader(CSVFile))
+        {
+            var headerLine = reader.ReadLine()!;
+            headers = headerLine.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                values.Add(line.Split(",", StringSplitOptions.None));
+            }
+
+        }
+        foreach (var header in headers)
+        {
+            Console.WriteLine($"{header}");
+        }
+
+
     }
 }
